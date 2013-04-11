6d4fada612be39b23f278f3a37da49c624ca06b4
JamesOfStiffRods <jmackin@rogers.com>
2013-04-11 15:58:45 -0400

    It's something... else

--- TheGateService/Sources/Endpoints/AdminPanelService.cs
+++ TheGateService/Sources/Endpoints/AdminPanelService.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
+using ServiceStack.ServiceHost;
 using ServiceStack.ServiceInterface;
 
 using TheGateService.Responses;
@@ -16,6 +17,22 @@ namespace TheGateService.Endpoints {
     public class AdminPanelService : Service {
         public object Get(AdminPanel request) {
             return new AdminPanelResponse { Results = new ProductDbProvider().GetAll() };
+        }
+
+        public object Post(AdminPanel request) {
+            Global.Log.Debug("Post");
+            foreach (var p in request.Products)
+                Global.Log.Debug(p.Id);
+
+            return null;
+        }
+
+        public object Delete(AdminPanel request) {
+            Global.Log.Debug("Delete");
+            var id = Request.FormData["id"];
+            Global.Log.Debug(id);
+
+            return null;
         }
     }
 }
