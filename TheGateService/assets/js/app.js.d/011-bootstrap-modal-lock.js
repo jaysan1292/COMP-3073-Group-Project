37aac4d6f750154cc369cf0661b77c25b31939e1
Jason Recillo <jaysan1292@gmail.com>
2013-04-13 14:45:02 -0400

    Admin panel product editing form (refs #36)

---
+++ TheGateService/assets/js/app.js.d/011-bootstrap-modal-lock.js
@@ -0,0 +1,54 @@
+﻿// Slightly modified from:
+// http://stackoverflow.com/questions/13421750/twitter-bootstrap-jquery-how-to-temporarily-prevent-the-modal-from-being-closed
+(function($) {
+    var _superModal = $.fn.modal;
+
+    $.extend(_superModal.defaults, {
+        locked: false
+    });
+
+    var Modal = function(element, options) {
+        _superModal.Constructor.apply(this, arguments);
+    };
+
+    Modal.prototype = $.extend({}, _superModal.Constructor.prototype, {
+        constructor: Modal,
+        _super: function() {
+            var args = $.makeArray(arguments);
+            _superModal.Constructor.prototype[args.shift()].apply(this, args);
+        },
+        lock: function() {
+            this.options.locked = true;
+        },
+        unlock: function() {
+            this.options.locked = false;
+        },
+        hide: function() {
+            if (this.options.locked) {
+                this.$element.trigger($.Event('hidePrevented'));
+                return;
+            }
+            this._super('hide');
+        },
+        hidePrevented: function() {
+        },
+    });
+
+    $.fn.modal = $.extend(function(option) {
+        var args = $.makeArray(arguments),
+            option = args.shift();
+
+        return this.each(function() {
+            var $this = $(this);
+            var data = $this.data('modal'),
+                options = $.extend({}, _superModal.defaults, $this.data(), typeof option == 'object' && option);
+
+            if (!data) {
+                $this.data('modal', (data = new Modal(this, options)));
+            }
+            if (typeof option == 'string') {
+                data[option].apply(data, args);
+            }
+        });
+    }, $.fn.modal);
+})(jQuery);
